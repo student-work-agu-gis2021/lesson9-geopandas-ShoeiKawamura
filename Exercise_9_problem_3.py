d782e225f382b8136c272156af7909f5b198854c
河村祥栄 <82557872+ShoeiKawamura@users.noreply.github.com>
2021-06-14 00:49:41 +0900

    Update Exercise_9_problem_3.py

--- Exercise_9_problem_3.py
+++ Exercise_9_problem_3.py
@@ -9,13 +9,14 @@
 # YOUR CODE HERE 1 to read data
 import geopandas as gpd
 from pyproj import CRS
-data=None
+data=gpd.read_file('Kruger_posts.shp')
+
 
 # - Check the crs of the input data. If this information is missing, set it as epsg:4326 (WGS84).
 # - Reproject the data from WGS84 to `EPSG:32735` -projection which stands for UTM Zone 35S (UTM zone for South Africa) to transform the data into metric system. (don't create a new variable, update the existing variable `data`!)"
 
 # YOUR CODE HERE 2 to set crs
-
+data = data.to_crs(epsg=32735)
 # CODE FOR TESTING YOUR SOLUTION
 
 # Check the data
@@ -30,8 +31,7 @@ print(data.crs)
 #  - Group the data by userid
 
 #  YOUR CODE HERE 3 to group 
-grouped=None
-
+grouped=data.groupby('userid')
 # CODE FOR TESTING YOUR SOLUTION
 
 #Check the number of groups:
@@ -44,7 +44,7 @@ assert len(grouped.groups) == data["userid"].nunique(), "Number of groups should
 # YOUR CODE HERE 4 to set movements
 import pandas as pd
 from shapely.geometry import LineString, Point
-movements=None
+movements=gpd.GeoDataFrame(columns=['userid', 'geometry'])
 # CODE FOR TESTING YOUR SOLUTION
 
 #Check the result
@@ -58,6 +58,7 @@ print(movements["geometry"].head())
 # - Calculate the lenghts of the lines into a new column called ``distance`` in ``movements`` GeoDataFrame.
 
 # YOUR CODE HERE 5 to calculate distance
+
 
 # CODE FOR TESTING YOUR SOLUTION
 
